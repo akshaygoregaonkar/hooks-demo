c488fc9252329eb993d587c9840ffa110a04318d
Akshay Goregaonkar <akshaygoregaonkar@gmail.com>
2021-07-21 11:39:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Dashboard from "./components/Component_prac_hooks/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard/>  
+    </div>
+  );
+}
+
+export default App;
